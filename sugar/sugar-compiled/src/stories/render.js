81b569aa813356b15fc4bb5ad4a2919ef02a8c2f
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2022-08-11 13:26:30 -0400

    chore: remove @singlestone/sugar-compiled (#57)

--- sugar/sugar-compiled/src/stories/render.js
+++
@@ -1,5 +0,0 @@
-export const renderAllStories = (stories) =>
-  Object.keys(stories)
-    .filter((story) => story !== "default")
-    .map((storyKey) => stories[storyKey](stories[storyKey].args))
-    .join("\n");
--- sugar/sugar-old-example/tailwind.config.js
+++ sugar/sugar-old-example/tailwind.config.js
@@ -1,12 +1,7 @@
 // const colors = require("tailwindcss/colors");
 module.exports = {
   mode: "jit",
-  content: [
-    "./src/**/*.js",
-    "./src/**/*.ts",
-    "./src/**/*.css",
-    "../sugar-old-example/*.html",
-  ],
+  content: ["./*.html"],
   theme: {
     extend: {
       colors: {},
