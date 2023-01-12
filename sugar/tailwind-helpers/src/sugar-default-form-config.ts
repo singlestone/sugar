258f91c63fec9c2e03d95e26b10b4d4064174685
Richard Harrah <topplethenunnery@gmail.com>
2023-01-12 18:17:02 -0500

    feat(checkboxes): creates checkbox styles

--- sugar/tailwind-helpers/src/sugar-default-form-config.ts
+++ sugar/tailwind-helpers/src/sugar-default-form-config.ts
@@ -3,4 +3,9 @@ import type { PluginAPI } from "tailwindcss/types/config";
 export const sugarDefaultFormConfig = (theme: PluginAPI["theme"]) => ({
   selectColorFamily: "accent",
   selectColorShade: theme("sugarDefaultColorShade"),
+  checkBoxRadioBorder: `1px solid ${theme("colors.neutral.400")}`,
+  checkBoxRadioCheckedColorFamily: "accent",
+  checkBoxRadioCheckedColorShade: "600",
+  checkBoxRadioHoverColorShade: "200",
+  checkBoxRadioDimensions: theme("spacing.7"),
 });
