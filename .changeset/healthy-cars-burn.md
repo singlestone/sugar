cad53d58ce5db65a29c24002053eae2d325a333b
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2023-02-03 18:13:31 -0500

    fix: make select chevron color change (#249)

---
+++ .changeset/healthy-cars-burn.md
@@ -0,0 +1,7 @@
+---
+"@singlestone/tailwind-plugin-form-sugar": patch
+"@singlestone/storybook-sugar": patch
+"@singlestone/sugar": patch
+---
+
+fix select chevron color
--- sugar/tailwind-plugin-form/src/inputs/select-input.styles.ts
+++ sugar/tailwind-plugin-form/src/inputs/select-input.styles.ts
@@ -9,6 +9,8 @@ export const selectInputStyles = (theme: PluginAPI["theme"]): CSSRuleObject => {
     theme("sugarFormControls.selectColorFamily"),
     theme("sugarFormControls.selectColorShade")
   );
+  // inline SVGs need to be HTML escaped, so we use %23 instead of #
+  const strokeColor = selectColor.replace("#", "%23");
   return {
     ...inputBase(theme),
     paddingRight: theme("spacing.9"),
@@ -18,7 +20,7 @@ export const selectInputStyles = (theme: PluginAPI["theme"]): CSSRuleObject => {
     backgroundRepeat: "no-repeat",
     backgroundColor: theme("colors.white"),
     border: `solid ${selectColor} ${theme("borderWidth.2")}`,
-    backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%233567e4' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e")`,
+    backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='${strokeColor}' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e")`,
     backgroundPosition: `right 12px center`,
     backgroundSize: `20px 15px`,
     whiteSpace: "nowrap",
