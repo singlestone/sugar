eeba23a0e5c00b2c8e4a57cbdfa33dd0100c8c76
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2023-02-03 22:38:57 -0500

    chore: fix vercel build (#252)

--- sugar/react/vite.config.ts
+++ sugar/react/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "example-dist",
+  },
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
