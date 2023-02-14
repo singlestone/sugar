3cd37af795706fb4c2ce6f4efe795a57da737221
Richard Harrah <topplethenunnery@gmail.com>
2023-02-13 19:31:09 -0500

    refactor: reorganize react example

--- sugar/react/example/App.tsx
+++ sugar/react/example/App.tsx
@@ -1,14 +1,11 @@
 import {
-  ALL_POSSIBLE_COLOR_VARIANTS_COMBINATIONS,
-  Button,
-  ButtonWithIcon,
-  IconOnlyButton,
-} from "../src";
-import { Checkmark } from "./Checkmark";
+  ButtonsPanel,
+  ButtonsWithIconsPanel,
+  IconOnlyButtonsPanel,
+} from "./panels/Button";
 import { CheckboxPanel } from "./panels/Checkbox";
 import { InputPanel } from "./panels/Input";
 import { SelectPanel } from "./panels/Select";
-import { getButtonDisplayValue } from "./utils";
 
 export const App = () => (
   <div className="flex flex-col min-h-screen">
@@ -20,65 +17,10 @@ export const App = () => (
       </div>
     </header>
     <main className="flex flex-1">
-      <div className="container mx-auto py-6 flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div className="flex flex-col">
-          <h1 className="flex-initial inline-block text-left text-base text-black text-lg tracking-wider leading-tight">
-            Buttons
-          </h1>
-          <div className="flex flex-grow flex-col gap-3 bg-white rounded-md overflow-hidden p-3 items-center">
-            {ALL_POSSIBLE_COLOR_VARIANTS_COMBINATIONS.map(
-              ({ color, variant }) => (
-                <Button
-                  className="whitespace-nowrap"
-                  color={color}
-                  variant={variant}
-                  key={`button-color-${color}-variant-${variant}`}
-                >
-                  {getButtonDisplayValue(color, variant)}
-                </Button>
-              )
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <h1 className="flex-initial inline-block text-left text-base text-black text-lg tracking-wider leading-tight">
-            Button with Icons
-          </h1>
-          <div className="flex flex-grow flex-col gap-3 bg-white rounded-md overflow-hidden py-3 items-center">
-            {ALL_POSSIBLE_COLOR_VARIANTS_COMBINATIONS.map(
-              ({ color, variant }) => (
-                <ButtonWithIcon
-                  className="whitespace-nowrap"
-                  color={color}
-                  variant={variant}
-                  key={`button-color-${color}-variant-${variant}`}
-                >
-                  <Checkmark />
-                  {getButtonDisplayValue(color, variant)}
-                </ButtonWithIcon>
-              )
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <h1 className="flex-initial inline-block text-left text-base text-black text-lg tracking-wider leading-tight">
-            Icon Only Buttons
-          </h1>
-          <div className="flex flex-grow flex-col gap-3 bg-white rounded-md overflow-hidden p-3 items-center">
-            {ALL_POSSIBLE_COLOR_VARIANTS_COMBINATIONS.map(
-              ({ color, variant }) => (
-                <IconOnlyButton
-                  color={color}
-                  variant={variant}
-                  key={`button-color-${color}-variant-${variant}`}
-                  title={getButtonDisplayValue(color, variant)}
-                >
-                  <Checkmark />
-                </IconOnlyButton>
-              )
-            )}
-          </div>
-        </div>
+      <div className="container mx-auto py-6 flex-1 grid grid-cols-1 auto-rows-min md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <ButtonsPanel />
+        <ButtonsWithIconsPanel />
+        <IconOnlyButtonsPanel />
         <SelectPanel />
         <InputPanel />
         <CheckboxPanel />
---
+++ sugar/react/example/panels/RadioGroup.tsx
@@ -0,0 +1,13 @@
+import { Checkbox, Label } from "../../src/Input";
+import { PanelBody, PanelHeading, PanelRoot } from "./Panel";
+
+export const RadioGroupPanel = () => {
+  return (
+    <PanelRoot>
+      <PanelHeading>RadioGroupPanel</PanelHeading>
+      <PanelBody>
+
+      </PanelBody>
+    </PanelRoot>
+  );
+};
