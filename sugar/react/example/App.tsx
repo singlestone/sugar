0d3880278986beb2e8de9d0c7a564d9405088f8b
Richard Harrah <rharrah@singlestoneconsulting.com>
2023-02-21 15:12:27 -0500

    chore: extract SideBySide panel

--- sugar/react/example/App.tsx
+++ sugar/react/example/App.tsx
@@ -1,4 +1,3 @@
-import { Label, Select, TextInput } from "../src";
 import {
   ButtonsPanel,
   ButtonsWithIconsPanel,
@@ -6,9 +5,9 @@ import {
 } from "./panels/Button";
 import { CheckboxPanel } from "./panels/Checkbox";
 import { InputPanel } from "./panels/Input";
-import { PanelBody, PanelHeading, PanelRoot } from "./panels/Panel";
 import { RadioGroupPanel } from "./panels/RadioGroup";
 import { SelectPanel } from "./panels/Select";
+import { SideBySidePanel } from "./panels/SideBySide";
 
 export const App = () => (
   <div className="flex flex-col min-h-screen">
@@ -28,26 +27,7 @@ export const App = () => (
         <InputPanel />
         <CheckboxPanel />
         <RadioGroupPanel />
-        <PanelRoot className="lg:col-span-2">
-          <PanelHeading>Side by Side Example</PanelHeading>
-          <PanelBody direction="row">
-            <div className="flex flex-1 flex-col items-start">
-              <Label htmlFor="sidy-by-side-select">Select</Label>
-              <Select defaultValue="" id="sidy-by-side-select">
-                <option disabled value="">
-                  Select an Option
-                </option>
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
-              </Select>
-            </div>
-            <div className="flex flex-1 flex-col items-start">
-              <Label htmlFor="sidy-by-side-text-input">Text Input</Label>
-              <TextInput id="sidy-by-side-text-input" />
-            </div>
-          </PanelBody>
-        </PanelRoot>
+        <SideBySidePanel />
       </div>
     </main>
   </div>
